1f04f2fbe1dff2ae32f7931ad73496c6bec14de4
hiwhatsupguys <114522489+hiwhatsupguys@users.noreply.github.com>
2025-07-25 22:56:55 -0700

    update to the circle fragment shader code to allow circle outline

--- GraphicsFromScratch/Content/Shaders/MyShaders/Circle.frag.hlsl
+++ GraphicsFromScratch/Content/Shaders/MyShaders/Circle.frag.hlsl
@@ -10,6 +10,7 @@ float4 main(Input input) : SV_Target0
 {
     const float2 circleCenter = float2(0.5, 0.5);
     const float radius = 0.5;
+    float fade = 0.005;
 
     // percent of radius
     float thickness = 0.2;
@@ -19,10 +20,12 @@ float4 main(Input input) : SV_Target0
     float distFromEdge = radius - dist;
 
 
+    // circle code: https://www.youtube.com/watch?v=xf7Y988cPRk 
     // this should probably be the other way around semantically
     // 0 if negative, 1 otherwise
-    float blend = smoothstep(0.0f, 0.005f, distFromEdge);
-    // float blend = step(0.0f, distFromEdge);
+    float blend = smoothstep(0.0f, fade, distFromEdge);
+    // other side blend
+    // blend *= 1.0 - smoothstep(thickness - fade, thickness, distFromEdge);
 
     // float4 outputColor = float4(input.Color.r, input.Color.g, input.Color.b, blend);
     float4 outputColor = float4(input.Color.rgb, blend);
