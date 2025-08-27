15adb748f5694ed4d8a5f58dd8b163d0741a80f4
hiwhatsupguys <114522489+hiwhatsupguys@users.noreply.github.com>
2025-08-26 18:20:17 -0700

    Releases texture and sampler

--- GraphicsFromScratch/Main.cpp
+++ GraphicsFromScratch/Main.cpp
@@ -469,6 +469,10 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+
+    SDL_ReleaseGPUTexture(device, texture);
+    SDL_ReleaseGPUSampler(device, sampler);
+
     SDL_ReleaseGPUBuffer(device, vertexBuffer);
     SDL_ReleaseGPUBuffer(device, indexBuffer);
 
